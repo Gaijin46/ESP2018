1f7a7a1f4bd8b4b32f95207b40a6af4c889d4cfa
root <root@DESKTOP-DAU4PGH.localdomain>
2018-12-21 14:26:39 +0100

    neis

---
+++ ass3.c
@@ -0,0 +1,20 @@
+//-----------------------------------------------------------------------------
+// ass3.c
+//
+// This program encrypts plain text. It it cycles each letter of the input
+// through the english alphabet, using the modulo of 256 divided 
+// by the input strings' length.
+// In this code, this number is called the 'key'.
+// 
+// Group: Group 02, study assistant Martin Ferdinand Haubenwallner
+//
+// Authors: Philipp Bardakji 11701628
+// Kenan Mujic 11771967
+//-----------------------------------------------------------------------------
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
