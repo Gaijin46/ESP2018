dd2e6862be9dbfe7b0d20cc0ebdc2ae63d8f859e
phibaa <root@DESKTOP-DAU4PGH.localdomain>
2018-12-21 17:59:27 +0100

    datei einlesen

--- ass3.c
+++ ass3.c
@@ -17,5 +17,52 @@
 #include <stdlib.h>
 #include <string.h>
 
+void main()
+{
+  FILE *fp;
+  char config[52][5];
+  char cards[26][3];
+  
+  int index;
+  int character;
+  
+  fp = fopen("config.txt", "r");
+  if(fp == NULL)
+  {
+	printf("[ERR] could not read file!");
+    return 1;	
+  }
+  for(index = 0; index < 52; index++)
+  {
+    fscanf(fp, "%s[]", &config[index]);
+  }
 
-saklakfjlas
+ //for(index =0; index < 5; index++)
+  //{
+	//if(config[index] == 32)
+    //{  
+      //config[index] = config[(index) + 1];
+      //config[index + 1] = "/0";
+	//}
+  //}
+
+  for(index = 0; index < 26; index++)
+  {	  
+    cards[index][0] = config[(index*2)][0];
+	cards[index][1] = "\0";
+    strcat(cards[index], config[((index*2)+1)]); 
+  }
+  for(index = 0; index < 52; index++)
+  {
+    printf("%s\n", config[index]);
+  }
+  
+  for(index = 0; index < 26; index++)
+  {
+    printf("%s\n", cards[index]);
+  }
+  
+  
+
+  fclose(fp);
+}
