974fa33b1b293fc8478dc77907a97cdc6b2ed757
phibaa <root@DESKTOP-DAU4PGH.localdomain>
2018-12-22 16:52:50 +0100

    neu

--- ass3.c
+++ ass3.c
@@ -6,7 +6,7 @@
 // by the input strings' length.
 // 
 // 
-// Group: Group 02, study assistant Martin Ferdinand Haubenwallner
+// Group: Group 02, study assistant Martin Haubenwallner
 //
 // Authors: Philipp Bardakji 11701628
 //          Kenan Mujic 11771967
@@ -17,47 +17,70 @@
 #include <stdlib.h>
 #include <string.h>
 
-void main()
+typedef struct _Card_
 {
-  FILE *fp;
+  char color_;
+  char value_;
+}Card;
+
+typedef enum _CardValue_
+{
+  A = 1,
+  J = 11,
+  Q = 12,
+  K = 13
+} CardValue;
+
+int main(int argc, char* argv[])
+{
+  FILE *file;
   char config[52][5];
   char cards[26][4];
-  
   int index;
-  int character;
+  int read;
   
-  fp = fopen("config.txt", "r");
-  if(fp == NULL)
+  
+  if(argc != 2)
   {
-	printf("[ERR] could not read file!");
+    printf("[ERR] Usage: ./ass3 [file-name]\n");
+    return 0;
+  }
+  
+  file = fopen(argv[1], "r");
+  //fread(&cardType, sizeof(struct _Card_), 1, file);
+  
+  if(file == NULL)
+  {
+	printf("[ERR] could not read file!\n");
     return 1;	
   }
   for(index = 0; index < 52; index++)
   {
-    fscanf(fp, "%s[]", &config[index]);
+    read = fscanf(file, "%s[]", &config[index]);
   }
 
- //for(index =0; index < 5; index++)
-  //{
-	//if(config[index] == 32)
-    //{  
-      //config[index] = config[(index) + 1];
-      //config[index + 1] = "/0";
-	//}
-  //}
-
-  for(index = 0; index < 26; index++)
-  {	  
-    cards[index][0] = config[(index*2)][0];
-	cards[index][1] = 0;
-    strcat(cards[index], config[((index*2)+1)]); 
-  }
-
-  for(index = 0; index < 26; index++)
+  if(read != 1)
+    printf("[ERR] Invalid file!\n");
+  else
   {
-    printf("%s\n", cards[index]);
+    for(index = 0; index < 26; index++)
+    {
+      struct Card(index);
+      Card.color(index) = config[(index*2)][0];
+	  Card.value(index) = config[((index*2)+1)];
+	}
+    //for(index = 0; index < 26; index++)
+    //{	  
+      //cards[index][0] = config[(index*2)][0];
+	  //cards[index][1] = 0;
+      //strcat(cards[index], config[((index*2)+1)]);
+    //}
+  
+    //for(index = 0; index < 26; index++)
+    //{
+      //printf("%s\n", cards[index]);
+    //}
   }
   
-
-  fclose(fp);
+  fclose(file);
 }
